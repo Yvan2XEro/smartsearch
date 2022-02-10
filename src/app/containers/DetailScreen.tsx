b92f48471547f564bf0c093b188cf4307c58f042
Yvan2Ero <yvankanaj@gmail.com>
2022-02-10 14:18:50 +0100

    Retouche du design de l'ecran de details

--- src/app/containers/DetailScreen.tsx
+++ src/app/containers/DetailScreen.tsx
@@ -2,12 +2,14 @@ import * as React from 'react';
 import {ScrollView, StyleSheet, View} from 'react-native';
 import {Document} from '../models/Document';
 import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import {Paragraph, Text } from 'react-native-paper';
 import DetailsHeader from '../components/DetailsHeader';
 import FiltersListModal from '../components/FiltersListModal';
 import { useSelector } from 'react-redux';
 import { settingSelector } from '../store/detailSetting/selectors';
 import { DetailSetting } from '../types';
+import { theme } from '../styles';
 
 const DetailScreen = ({route, navigation}: {route: any; navigation: any}) => {
   /* 2. Get the param */
@@ -25,34 +27,18 @@ const DetailScreen = ({route, navigation}: {route: any; navigation: any}) => {
       />
       <View
         style={{
-          margin: 5,
+          marginHorizontal: 10,
+          marginTop: 10,
+          paddingBottom: 5
         }}>
         <ScrollView style={{marginBottom: 85}}>
-          {document.openaccess && (
-            <View
-              style={{
-                width: 100,
-                padding: 5,
-                borderRadius: 5,
-                marginBottom: 20,
-                alignSelf: 'center',
-                backgroundColor: 'green',
-              }}>
-              <Entypo
-                name="open-book"
-                color="white"
-                size={25}
-                style={{alignSelf: 'center'}}
-              />
-              <Text
-                style={{
-                  fontSize: 10,
-                  fontWeight: 'bold',
-                  textAlign: 'center',
-                  color: 'white',
-                }}>
-                Open access!
-              </Text>
+          {settings.openaccess && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Open access:</Text>
+              <View>
+                {document.openaccess?<Entypo name="check" size={30} color={theme.colors.primary} />:
+                <FontAwesome5 name="times" size={30} color={theme.colors.error} />}
+              </View>
             </View>
           )}
           <View style={styles.row}>
@@ -61,41 +47,51 @@ const DetailScreen = ({route, navigation}: {route: any; navigation: any}) => {
               <Text style={styles.content}>{document.title}</Text>
             </View>
           </View>
-          {settings.doi && <View style={styles.row}>
-            <Text style={styles.label}>Doi: </Text>
-            <View style={{flex: 1}}>
-              <Text style={styles.content}>{document.doi}</Text>
+          {settings.doi && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Doi: </Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.content}>{document.doi}</Text>
+              </View>
             </View>
-          </View>}
-          {settings.publicationDate && <View style={styles.row}>
-            <Text style={styles.label}>Publicate Date: </Text>
-            <View style={{flex: 1}}>
-              <Text style={styles.content}>{document.publicationDate}</Text>
+          )}
+          {settings.publicationDate && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Publicate Date: </Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.content}>{document.publicationDate}</Text>
+              </View>
             </View>
-          </View>}
-          {settings.publisher&&<View style={styles.row}>
-            <Text style={styles.label}>Publisher: </Text>
-            <View style={{flex: 1}}>
-              <Text style={styles.content}>{document.publisher}</Text>
+          )}
+          {settings.publisher && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Publisher: </Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.content}>{document.publisher}</Text>
+              </View>
             </View>
-          </View>}
-          {settings.authors &&<View style={styles.row}>
-            <Text style={styles.label}>Authors: </Text>
-            <View style={{flex: 1}}>
-              <Text style={styles.content}>
-                {document.authors?.reduce(
-                  (acc, item) => acc + ', ' + item.creator,
-                  '',
-                )}
-              </Text>
+          )}
+          {settings.authors && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Authors: </Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.content}>
+                  {document.authors?.reduce(
+                    (acc, item) => acc + ', ' + item.creator,
+                    '',
+                  )}
+                </Text>
+              </View>
             </View>
-          </View>}
-          {settings.contentType &&<View style={styles.row}>
-            <Text style={styles.label}>Type :</Text>
-            <View style={{flex: 1}}>
-              <Text style={styles.content}>{document.contentType}</Text>
+          )}
+          {settings.contentType && (
+            <View style={styles.row}>
+              <Text style={styles.label}>Type :</Text>
+              <View style={{flex: 1}}>
+                <Text style={styles.content}>{document.contentType}</Text>
+              </View>
             </View>
-          </View>}
+          )}
           <View>
             <Text style={styles.label}>Abstract :</Text>
             <Paragraph style={{flex: 1}}>{document.abstract}</Paragraph>
@@ -116,7 +112,7 @@ export default DetailScreen;
 const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
-    marginTop: 4,
+    marginTop: 6,
   },
   label: {
     fontWeight: 'bold',
@@ -124,5 +120,6 @@ const styles = StyleSheet.create({
   },
   content: {
     marginHorizontal: 3,
+    fontSize: 16,
   },
 });
